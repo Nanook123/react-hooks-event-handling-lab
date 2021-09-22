a97845d98a26913b4dd31cb16ffb679833afc867
Joshua Allford <joshallfordnanook@gmail.com>
2021-09-22 22:44:26 +0100

    Assignemnt Complete!

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+
+
+
+
+    return (
+        <button onBlur={() =>
+            console.log("Hey! Eyes on me!")} onFocus={() =>
+                console.log("Good!")} name="EyesOnMe">Eyes on me</button>
+
+    )
+}
+
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,27 @@
-// Code Keypad Component Here
+
+
+
+function Keypad(){
+function handleSubmit(e){
+    e.preventDefault();
+    console.log("Entering password...");
+}
+    return(
+        
+
+
+
+
+        
+    <form name="password" onChange={handleSubmit}>
+        <input type="password" name="password" placeholder="enter password..."/>
+        <button>Enter Password</button>
+
+
+    </form>
+    
+    
+    )
+}
+
+export default Keypad
